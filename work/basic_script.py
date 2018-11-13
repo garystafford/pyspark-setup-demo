8e682b540d775f7cd8c2eb23e7876b14b67e57be
Gary A. Stafford <garystafford@rochester.rr.com>
2018-11-12 19:07:34 -0500

    Refactor project, add basic script

---
+++ work/basic_script.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import random
+
+technologies = ['PySpark', 'Python', 'Spark', 'Scala', 'Project Jupyter', 'PostgreSQL']
+print(technologies)
+
+technologies.sort()
+print(technologies)
+
+print("I'm interested in learning", random.choice(technologies))
---
+++ work/basket_dataframes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+
+spark = SparkSession \
+    .builder \
+    .appName('pyspark_demo_app') \
+    .config('spark.driver.extraClassPath',
+            '/home/garystafford/work/postgresql-42.2.5.jar') \
+    .getOrCreate()
+
+sc = spark.sparkContext
+
+bakery_schema = StructType([
+    StructField('date', StringType(), True),
+    StructField('time', StringType(), True),
+    StructField('transaction', IntegerType(), True),
+    StructField('item', StringType(), True)
+])
+
+df3 = spark.read \
+    .format("csv") \
+    .option("header", "true") \
+    .load("BreadBasket_DMS.csv", schema=bakery_schema)
+
+df3.show(10)
+df3.count()
