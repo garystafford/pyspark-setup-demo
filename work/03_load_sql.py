ad9bcf9e4fbb6618699e86382c7d04700e511a12
Gary Stafford <gary.a.stafford@gmail.com>
2019-09-28 11:11:22 -0700

    Update comments

--- work/03_load_sql.py
+++ work/03_load_sql.py
@@ -5,7 +5,7 @@ import psycopg2
 # source: https://stackoverflow.com/questions/45805871/python3-psycopg2-execute-sql-file
 
 # connect_str = 'host=localhost port=5432 dbname=demo user=postgres password=postgres1234' # for local testing
-connect_str = 'host=postgres port=5432 dbname=demo user=postgres password=postgres1234'
+connect_str = 'host=postgres port=5432 dbname=demo user=postgres password=postgres1234' # for docker
 conn = psycopg2.connect(connect_str)
 conn.autocommit = True
 cursor = conn.cursor()
