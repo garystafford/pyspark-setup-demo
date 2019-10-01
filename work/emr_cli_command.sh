981de1dfad6ba7cf49d76c3d76c8f69ff846e8c3
Gary Stafford <gary.a.stafford@gmail.com>
2019-10-01 09:04:20 -0700

    Simplify script

--- work/emr_cli_command.sh
+++ work/emr_cli_command.sh
@@ -14,11 +14,11 @@ aws emr create-cluster \
   --termination-protected \
   --applications Name=Hadoop Name=Hive Name=Pig Name=Hue Name=Spark Name=Presto Name=Flink \
   --ebs-root-volume-size 20 \
-  --ec2-attributes "{\"KeyName\":\"${KEY_NAME}\",
-    \"InstanceProfile\":\"EMR_EC2_DefaultRole\",
-    \"SubnetId\":\"${SUBNET_ID}\",
-    \"EmrManagedSlaveSecurityGroup\":\"${EMR_MANAGED_SLAVE_SECURITY_GROUP}\",
-    \"EmrManagedMasterSecurityGroup\":\"${EMR_MANAGED_MASTER_SECURITY_GROUP}\"}" \
+  --ec2-attributes '{"KeyName":"${KEY_NAME}",
+    "InstanceProfile":"EMR_EC2_DefaultRole",
+    "SubnetId":"${SUBNET_ID}",
+    "EmrManagedSlaveSecurityGroup":"${EMR_MANAGED_SLAVE_SECURITY_GROUP}",
+    "EmrManagedMasterSecurityGroup":"${EMR_MANAGED_MASTER_SECURITY_GROUP}"}' \
   --service-role EMR_DefaultRole \
   --enable-debugging \
   --release-label emr-5.27.0 \
