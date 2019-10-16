1791cc05f6dff4758b2c8bc0bed29eed875522f8
Gary Stafford <gary.a.stafford@gmail.com>
2019-10-15 23:24:32 -0700

    Refactor 03 script

--- work/03_load_sql.py
+++ work/03_load_sql.py
@@ -6,9 +6,7 @@ import psycopg2
 # source: https://stackoverflow.com/questions/45805871/python3-psycopg2-execute-sql-file
 
 # connect_str = 'host=localhost port=5432 dbname=demo user=postgres password=postgres1234' # local testing
-# connect_str = 'host=postgres port=5432 dbname=demo user=postgres password=postgres1234' # Docker
-connect_str = 'host=demo-postgres.cxauxtdppqiu.us-east-1.rds.amazonaws.com port=5432 dbname=postgres user=postgres password=secret'  # AWS RDS Public
-# connect_str = 'host=rds-share-db-demo.cxauxtdppqiu.us-east-1.rds.amazonaws.com port=5432 dbname=postgres user=postgres password=secret'  # AWS RDS Private
+connect_str = 'host=postgres port=5432 dbname=demo user=postgres password=postgres1234' # Docker
 
 conn = psycopg2.connect(connect_str)
 conn.autocommit = True
