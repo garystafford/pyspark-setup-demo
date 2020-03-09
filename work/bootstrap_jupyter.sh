8eca64ec44be3fb2cd85060ca53cdb1363de9334
Gary Stafford <gary.a.stafford@gmail.com>
2020-03-08 22:16:19 -0700

    Refactor script

--- work/bootstrap_jupyter.sh
+++ work/bootstrap_jupyter.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+# update/upgrade and install htop
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get install htop
+
 # install required python packages
 python3 -m pip install --user --upgrade pip
 python3 -m pip install -r requirements.txt --upgrade
@@ -16,7 +20,3 @@ fi
 
 # spark-submit logging level from INFO to WARN
 sudo cp log4j.properties /usr/local/spark/conf/log4j.properties
-
-# update/upgrade and install htop
-sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt-get install htop
